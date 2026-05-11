8ee8410b7abbc561f5838d3d58e2ad4028d5aa2e
MnFeN <self.mail.address@gmail.com>
2026-05-12 05:54:47 +0800

    cleanup

--- Simulant/Simulation/SimPresetBase.cs
+++ Simulant/Simulation/SimPresetBase.cs
@@ -16,7 +16,12 @@ namespace Simulant.Simulation
         public abstract PhaseData Phase { get; }
         public abstract Type SimLogicType { get; }
         public abstract string Description { get; }
-        public virtual IEnumerable<SimOptionBase> Options => Enumerable.Empty<SimOptionBase>();
+
+        /// <summary> 全部 Option，包括纯 Label 这种仅用于显示 UI 的假 Option。</summary>
+        public List<SimOptionBase> Options { get; protected set; } = new List<SimOptionBase>();
+
+        /// <summary> 设置了 PropertyName 的 Option，应该均为绑定属性的 <see cref="SimOption{T}"/>。 </summary>
+        public Dictionary<string, SimOptionBase> NamedOptions { get; protected set; } = new Dictionary<string, SimOptionBase>();
 
         public override string ToString() => $"[模拟] {Name}";
         public SimLogicBase CreateSimLogic()
@@ -25,9 +30,17 @@ namespace Simulant.Simulation
             return SimLogicType == null ? null : (SimLogicBase)Activator.CreateInstance(SimLogicType);
         }
 
-        protected void ApplyOptions()
+        protected void AddOption(SimOptionBase option)
+        {
+            if (option == null) throw new NullReferenceException(nameof(option));
+            Options.Add(option);
+            if (!string.IsNullOrEmpty(option.PropertyName))
+                NamedOptions[option.PropertyName] = option;
+        }
+
+        internal void ApplyOptions()
         { 
-            foreach (var option in Options)
+            foreach (var option in NamedOptions.Values)
             {
                 option.ApplyTo(this);
             }
